c89c627d12402d25a76f952b72deafcd609d50d8
Gohar <aria.g@mail.ru>
2016-11-19 18:44:09 +0400

    working with objects

---
+++ grammar-and-types/workwithobjects.js
@@ -0,0 +1,43 @@
+/**
+ * Topic: getting value from the object
+ * Problem instructions: Follow instructions in comments
+ * Relevant resources:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types
+ *
+ */
+
+
+'use strict'
+
+
+function problem1(){
+  // code for problem1
+var sth1 = {a: 'one', b: 'two'};
+console.log(sth1(a))};  //  -replace brackets
+
+problem1();
+
+
+
+function problem2(){
+  // code for problem2S
+var sth2 = {c: 'one', d: 'two'};
+console.log(sth[c])  // - change syntax
+};
+
+problem2();
+
+
+/**
+ * Solution - below is a base64 blob of the solution.
+ *
+--begin--
+DQpmdW5jdGlvbiBwcm9ibGVtMSgpew0KICAvLyBjb2RlIGZvciBwcm9ibGVt
+MQ0KdmFyIHN0aDEgPSB7YTogJ29uZScsIGI6ICd0d28nfTsNCmNvbnNvbGUu
+bG9nKHN0aDEuYSl9OyAgDQoNCnByb2JsZW0xKCk7DQoNCg0KDQpmdW5jdGlv
+biBwcm9ibGVtMigpew0KICAvLyBjb2RlIGZvciBwcm9ibGVtMQ0KdmFyIHN0
+aDIgPSB7YzogJ29uZScsIGQ6ICd0d28nfTsNCmNvbnNvbGUubG9nKHN0aFsn
+YyddKQ0KfTsNCg0KcHJvYmxlbTIoKTs=
+--end--
+ *
+ */
